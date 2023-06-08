08ca4e9f988493e8f71cbf9b6eb245c71d19f8d6
Jose Lopez <roddriqo@gmail.com>
2023-06-07 23:33:54 -0400

    boton de salir

--- modelo/bd.php
+++ modelo/bd.php
@@ -1,11 +1,6 @@
 <?php
+$conexion = mysqli_connect( "localhost", "root" , "1234", "sistema_cobro_agua");
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "sistema_cobro_agua";
-
-$conexion = mysqli_connect($host, $user, $password, $database);
 if(!$conexion){
 echo "No se realizo la conexion a la basa de datos, el error fue:".
 mysqli_connect_error() ;
--- vista/estructura/superior.php
+++ vista/estructura/superior.php
@@ -18,9 +18,11 @@ error_reporting(0);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>OTB Patito</title>
     <link href='https://unpkg.com/css.gg@2.0.0/icons/css/atlasian.css' rel='stylesheet'>
+    <link rel="shortcut icon"  href="../complementos/img/logo.ico">
     <script src="https://kit.fontawesome.com/189cbc8fac.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../complementos/css/bootstrap.min.css">
     <link rel="stylesheet" href="../complementos/css/superior.css">
+    <link rel="stylesheet" href="../complementos/css/bootstrap.min.css">
+
     <script defer src="../vista/js/bootstrap.bundle.min.js"></script>
   </head>
   <body>
@@ -49,11 +51,27 @@ error_reporting(0);
         <a class="btn-routes" href="{{ route('cobrar') }}"><div>Realizar <br> cobro</div></a>
         </section>
 
-        <section class="routes-content">
+        <!-- <section class="routes-content">
           <i class="fas fa-light fa-scroll"></i>
         <a class="btn-routes" href="">Historial de pagos</a>
-        </section>
+        </section> -->
 
-      </section> 
+      </section>
+
+      <section style="display: flex; gap: 10px; width: 100%; justify-content: center; margin-top: 25px">
+        <div>
+          <img src="../complementos/img/avatar.jpg" width="40px" height="40px" 
+          style="border-radius: 50%; margin-top: 8px;">
+        </div>
+
+        <form style="display: flex; flex-direction: column; gap: 5px;" action="../modelo/login/cerrar.php" method="POST">
+          <label style="color: white;">Jose Rodrigo</label>
+          <button type="submit" class="btn btn-danger"
+            style="--bs-btn-padding-y: .15rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem; background-color: rgb(222, 0, 0);">
+            Salir
+          </button>
+        </form>
+
+      </section>
 
     </nav>
